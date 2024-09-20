a867f7d3fa0e590ac605b431adaf057beda3f225
disendra <Avchamps1@gmail.com>
2024-09-21 00:02:10 +0530

    21/9/2024

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,14 +1,6 @@
 import { ChangeDetectorRef, Component, Input, Renderer2, TemplateRef, ViewChild, ElementRef } from '@angular/core';
 import { UserServicesService } from '../services/user-services.service';
 import { FaServiceService } from '../services/fa-service.service';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import listPlugin from '@fullcalendar/list';
-import { Chart, registerables } from 'chart.js';
-import { CalendarOptions, EventApi, EventClickArg } from '@fullcalendar/core';
-
-declare var bootstrap: any; // Declare Bootstrap globally
 
 @Component({
   selector: 'app-calender',
@@ -16,23 +8,19 @@ declare var bootstrap: any; // Declare Bootstrap globally
   styleUrls: ['./calender.component.css']
 })
 export class CalenderComponent {
-  totalScore: number = 0;
-  chart: any;
   startDate: any;
   endDate: any;
-  displaySelector: boolean = false;
-  calendarVisible = true; // Default to true
-  showChart: boolean = false;
-  isCalender: boolean = false;
-  filteredEvents: any[] = [];
-  selectedEventTypes: string[] = [];
-  currentEvents: EventApi[] = [];
-  events: any[] = [];
+  eventName: any;
+  eventUrl: any;
+  uniqueEventTypes: any;
+  displayEventName: string = '';
+  eventColorCode: string = '';
+  eventNameInput: any;
   showSpinner: boolean = true;
   selectedEventType: any;
-  @Input() toolType: any;
+  eventsResponse: any[] = [];
+  filteredEvents: any[] = [];
   @ViewChild('eventInfoModal') eventInfoModal!: ElementRef;
-  @ViewChild('eventTypeModal') eventTypeModal!: ElementRef;
 
   eventTypes = [
     { displayName: 'Webinar', value: 'webinar' },
@@ -43,222 +31,141 @@ export class CalenderComponent {
   ];
 
   constructor(
-    private cdr: ChangeDetectorRef,
     private faService: FaServiceService,
     public userService: UserServicesService,
-    private renderer: Renderer2
-  ) {
-    Chart.register(...registerables);
-    this.handleCreateEventClick = this.handleCreateEventClick.bind(this);
-  }
+  ) { }
 
   ngOnInit(): void {
-    this.handleMessageChange();
     this.getEvents();
   }
 
-  handleMessageChange() {
-    if (this.toolType === 'displaySelector') {
-      this.displaySelector = true;
-    } else if (this.toolType === 'calender') {
-      this.isCalender = true;
-    }
+  // Filter events by the event type
+  filterByEventType(eventType: string, eventName: any, colorCode: any): void {
+    this.displayEventName = eventName;
+    this.eventColorCode = colorCode;
+    this.filteredEvents = this.eventsResponse.filter(event => event.eventType === eventType);
   }
 
-  // Method to show the Event Information modal
-  showEventInfoModal(): void {
-    const modalElement = new bootstrap.Modal(this.eventInfoModal.nativeElement);
-    modalElement.show();
-  }
-
-  // Method to show the Event Type Filter modal
-  showEventTypeModal(): void {
-    const modalElement = new bootstrap.Modal(this.eventTypeModal.nativeElement);
-    modalElement.show();
-  }
-
-  // Example form submission method
-  submitForm(
-    eventName: string,
-    eventUrl: string,
-    startDate: Date,
-    endDate: Date,
-    selectedEventType: string
-  ) {
-    const data = {
-      eventName: eventName,
-      eventUrl: eventUrl,
-      startDate: startDate,
-      endDate: endDate,
-      eventType: selectedEventType
-    };
-    this.faService.postEvent(data).subscribe(response => {
-      console.log(response);
-      this.showEventInfoModal(); // Show confirmation or reset modal
+  // Filter today's events
+  filterTodayEvents(): void {
+    const today = new Date().setHours(0, 0, 0, 0);  // Set to the start of the day
+    this.filteredEvents = this.eventsResponse.filter(event => {
+      const eventDate = new Date(event.event_date).setHours(0, 0, 0, 0);
+      return eventDate === today;
     });
+    console.log("Today's Events: ", this.filteredEvents);
   }
 
-  // Fetch events for the calendar
+
   getEvents() {
     this.showSpinner = true;
     this.faService.getEvents().subscribe((response: any) => {
-      console.log('Response from server:', response);
-      const newEvents = response.records.map((record: any) => ({
-        title: record.event_name,
-        start: record.event_date,
-        url: record.website_Url,
-        type: record.eventType
-      }));
-      console.log('Parsed events:', newEvents); // Log parsed events
-      this.events = newEvents;
-      this.filteredEvents = newEvents;
-      this.updateCalendarEvents();
+      if (response.records && Array.isArray(response.records)) {
+        this.eventsResponse = response.records;
+
+        // Populate unique event types
+        const eventTypes = new Set(this.eventsResponse.map((event: { eventType: any; }) => event.eventType));
+        const uniqueEventTypesArray = Array.from(eventTypes);
+        this.uniqueEventTypes = [];
+
+        uniqueEventTypesArray.forEach((eventType: string | null) => {
+          let colorCode = '';
+          const normalizedEventType = eventType ? eventType.toLowerCase() : null;
+          let eventName;
+
+          if (normalizedEventType === 'trade_show') {
+            colorCode = '#ff0000';
+            eventName = 'TradeShow';
+          } else if (normalizedEventType === 'webinar') {
+            colorCode = '#007bff';
+            eventName = 'Webinar';
+          } else if (normalizedEventType === 'classroom') {
+            colorCode = '#28a745';
+            eventName = 'ClassRoom';
+          } else if (normalizedEventType === 'online') {
+            colorCode = '#ffc107';
+            eventName = 'Online';
+          } else if (normalizedEventType === 'new_launch') {
+            colorCode = '#6f42c1';
+            eventName = 'NewLaunch';
+          } else if (!normalizedEventType) {
+            return;
+          } else {
+            return;
+          }
+
+          this.uniqueEventTypes.push({ eventType: eventType, name: eventName, colorCode: colorCode });
+        });
+
+        this.filterByEventType('trade_show', 'TradeShow', '#ff0000');
+
+        console.log(this.uniqueEventTypes);
+
+      } else {
+        console.error('Unexpected response format:', response);
+      }
+
+      this.showSpinner = false;
+    }, error => {
+      console.error('Error fetching events:', error);
       this.showSpinner = false;
     });
   }
 
-  updateCalendarEvents() {
-    this.calendarOptions.events = this.filteredEvents;
-    this.cdr.detectChanges();
+
+  getMonth(dateString: string): string {
+    const date = new Date(dateString);
+    return date.toLocaleString('default', { month: 'long' });
   }
 
-  handleCalendarToggle() {
-    this.calendarVisible = !this.calendarVisible;
+  getDay(dateString: string): string {
+    const date = new Date(dateString);
+    return date.getDate().toString();
   }
 
-  handleEventDidMount(info: any) {
-    const eventType = info.event.extendedProps.type;
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    return `${date.getDate()}${this.getOrdinal(date.getDate())} ${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()}`;
+  }
 
-    const eventColors: { [key: string]: string } = {
-      'webinar': '#F19ED2',
-      'trade_show': '#FF7F3E',
-      'classroom': '#7D8ABC',
-      'product_launch': '#379777',
-      'online': '#50B498'
+  getOrdinal(day: number): string {
+    if (day > 3 && day < 21) return 'th';
+    switch (day % 10) {
+      case 1: return 'st';
+      case 2: return 'nd';
+      case 3: return 'rd';
+      default: return 'th';
+    }
+  }
+
+  submitForm() {
+    const data = {
+      eventName: this.eventName,
+      eventUrl: this.eventUrl,
+      startDate: this.startDate,
+      endDate: this.endDate,
+      eventType: this.selectedEventType,
     };
 
-    const backgroundColor = eventColors[eventType] || 'gray';
-    info.el.style.backgroundColor = backgroundColor;
-    info.el.style.border = 'none';
-    info.el.style.color = 'white';
-    console.log('Mounted event:', info.event.title, 'with type:', eventType, 'and color:', backgroundColor);
-  }
-
-  calendarOptions: CalendarOptions = {
-    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
-    headerToolbar: {
-      left: 'prev,next today createEventButton EventType',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-    },
-    views: {
-      dayGridMonth: {
-        buttonText: 'month'
+    this.faService.postEvent(data).subscribe({
+      next: (response) => {
+        console.log('Event submitted successfully:', response);
       },
-      timeGridWeek: {
-        buttonText: 'week'
+      error: (err: any) => {
+        console.error('Error submitting the event:', err);
+        this.handleError(err);
       },
-      timeGridDay: {
-        buttonText: 'day'
-      },
-      listWeek: {
-        buttonText: 'list'
-      }
-    },
-    initialView: 'dayGridMonth',
-    weekends: true,
-    editable: true,
-    selectable: true,
-    selectMirror: true,
-    dayMaxEvents: true,
-    events: this.filteredEvents,
-    eventClick: this.handleEventClick.bind(this),
-    customButtons: {
-      createEventButton: {
-        text: 'Create Event',
-        click: () => this.showEventInfoModal()
-      },
-      EventType: {
-        text: 'Event Type',
-        click: () => this.showEventTypeModal()
-      }
-    },
-    eventDidMount: this.handleEventDidMount.bind(this)
-  };
-
-  handleCreateEventClick = () => {
-    this.showEventInfoModal();
-  }
-
-  toggleEventTypeDropdown() {
-    this.showEventTypeModal();
-  }
-
-  handleEventClick(info: EventClickArg): void {
-    if (info.event.url) {
-      window.open(info.event.url, '_blank');
-      info.jsEvent.preventDefault();
-      return;
-    }
-  }
-
-  onEventTypeChange(event: any) {
-    const value = event.target.value;
-    if (event.target.checked) {
-      this.selectedEventTypes.push(value);
-    } else {
-      this.selectedEventTypes = this.selectedEventTypes.filter(type => type !== value);
-    }
-  }
-
-  applyEventTypeFilter() {
-    if (this.selectedEventTypes.length === 0) {
-      this.filteredEvents = this.events;
-    } else {
-      this.filteredEvents = this.events.filter(event => this.selectedEventTypes.includes(event.type));
-    }
-    this.updateCalendarEvents();
-  }
-
-  calculateTotal() {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    let score = 0;
-    checkboxes.forEach((checkbox: any) => {
-      if (checkbox.checked) {
-        score++;
-      }
     });
-    this.totalScore = score;
-    this.showChart = true;
-    this.cdr.detectChanges();
-    this.createOrUpdateChart();
   }
 
-  createOrUpdateChart() {
-    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
-    if (this.chart) {
-      this.chart.data.datasets[0].data = [this.totalScore, this.getTotalCheckboxes() - this.totalScore];
-      this.chart.update();
-    } else if (ctx) {
-      this.chart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-          labels: ['Checked', 'Unchecked'],
-          datasets: [{
-            data: [this.totalScore, this.getTotalCheckboxes() - this.totalScore],
-            backgroundColor: ['green', 'red']
-          }]
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false
-        }
-      });
+  handleError(err: any) {
+    if (err.status === 400) {
+      console.log('Validation error:', err.error);
+    } else if (err.status === 500) {
+      console.log('Server error, please try again later.');
+    } else {
+      console.log('Unexpected error:', err);
     }
-  }
-
-  getTotalCheckboxes(): number {
-    return document.querySelectorAll('input[type="checkbox"]').length;
   }
 
   onBack() {
